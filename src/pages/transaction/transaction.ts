831b03833782bce0c6377d7151eef802ff950cb2
Jamie Suliivan <jamiesullivan523@gmail.com>
2017-09-28 14:17:38 +0100

    Edited Transaction Page

--- src/pages/transaction/transaction.ts
+++ src/pages/transaction/transaction.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Transaction } from '../../models/transaction-model';
 
-
-@IonicPage()
+@IonicPage({
+  name: 'TransactionPage',
+  segment: 'transaction'
+})
 @Component({
   selector: 'page-transaction',
   templateUrl: 'transaction.html',
 })
 export class TransactionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  transaction = {} as Transaction;
+  type: any;
+  amount: number;
+  notes: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TransactionPage');
-  }
 
 }
